f7bd76ab5fdb194e1f74ba1c0554fd347052f725
Mughalsahab <144914285+Mughalsahab@users.noreply.github.com>
2024-04-17 14:13:57 +0500

    Bye

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
     render() {
@@ -8,44 +7,53 @@ export class Navbar extends Component {
             <nav className={`navbar navbar-expand-lg bg-${this.props.mode} navbar-${this.props.mode}`}>
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">NewsGen</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" data-bs-toggle="offcanvas" href="#offcanvasExample" ariaControls="offcanvasExample">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link" aria-current="page">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/about" className="nav-link">About</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/business" className="nav-link">Business</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/entertainment" className="nav-link">Entertainment</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link">General</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/health" className="nav-link">Health</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/science" className="nav-link">Science</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/sport" className="nav-link">Sports</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/tech" className="nav-link">Technology</Link>
-                            </li>
-                        </ul>
-                        <div className="form-check form-switch" style={{
-                            marginTop: '0.5rem'
-                        }}>
-                            <input className="form-check-input" type="checkbox" role="switch" id="darkMode" onClick={this.props.toggleMode}/>
-                            <label className={`form-check-label text-${this.props.mode === 'dark' ? 'light' : 'dark' }`} htmlFor="darkMode">{`${this.props.mode === 'light' ? 'Enable Dark Mode' : 'Disable Dark Mode'}`}</label>
+                    <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" ariaLabelledby="offcanvasExampleLabel">
+                        <div className={`offcanvas-header bg-${this.props.mode}`}>
+                            <h5 className={`offcanvas-title text-${this.props.mode === 'dark' ? 'light' : 'dark'}`} id="offcanvasRightLabel">Please specify your choice</h5>
+                            <button type="button" className="btn-close" dataBsDismiss="offcanvas" ariaLabel="Close" style={{backgroundColor: 'red'}}></button>
+                        </div>
+                        <div className={`offcanvas-body bg-${this.props.mode} text-${this.props.mode === 'dark' ? 'light' : 'dark'}`}>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link" aria-current="page">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/about" className="nav-link">About</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/business" className="nav-link">Business</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/entertainment" className="nav-link">Entertainment</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link">General</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/health" className="nav-link">Health</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/science" className="nav-link">Science</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/sport" className="nav-link">Sports</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/tech" className="nav-link">Technology</Link>
+                                </li>
+                            </ul>
+                            <hr />
+                            <h5 className={`offcanvas-title d-lg-none d-inline-block text-${this.props.mode === 'dark' ? 'light' : 'dark'}`} id="offcanvasRightLabel">Mode</h5>
+                            <hr />
+                            <div className="form-check form-switch" style={{
+                                marginTop: '0.5rem'
+                            }}>
+                                <input className="form-check-input" type="checkbox" role="switch" id="darkMode" onClick={this.props.toggleMode} />
+                                <label className={`form-check-label text-${this.props.mode === 'dark' ? 'light' : 'dark'}`} htmlFor="darkMode">{`${this.props.mode === 'light' ? 'Enable Dark Mode' : 'Disable Dark Mode'}`}</label>
+                            </div>
                         </div>
                     </div>
                 </div>
