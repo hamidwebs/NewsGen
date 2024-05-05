0f918307455f8065df41a4705e0a76a963ddd00b
Hamid Raza <itmughal2023@gmail.com>
2024-05-05 17:04:46 +0500

    run npm i @emailjs/browser and sending email functionality in Contact.js by usinf emailjs

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Feedback from './Feedback';
+import emailjs from '@emailjs/browser';
 
 export default function Contact(props) {
+    const form = useRef();
     const [isSubmit, setIsSubmit] = useState(false)
-    const showFeedback = (e) => {
+    const [detail, setDetails] = useState({ from_name: '', lname: '', user_email: '', message: '' })
+    const { mode } = props;
+    const showFeedback = async (e) => {
         e.preventDefault();
         setIsSubmit(true);
+        await emailjs.sendForm('service_636t68d', 'template_gd7ktrl', form.current, {
+            publicKey: 'PxS4WrnZQyi4rtcbH'
+        })
     }
-    const [detail, setDetails] = useState({fname:'', lname:'', email:'', feedback:''})
-    const { mode } = props;
     const onChange = (e) => {
-        setDetails({...detail, [e.target.name]: e.target.value});
+        setDetails({ ...detail, [e.target.name]: e.target.value });
     }
     return (
         <div className='container mt-5'>
             <h1 className={`mb-4 ${mode === 'dark' ? 'text-light' : 'text-dark'}`}>Contact Us</h1>
-            <form className="row g-3" onSubmit={showFeedback}>
+            <form ref={form} className="row g-3" onSubmit={showFeedback}>
                 <div className={`mb-3`}>
                     <label htmlFor="enterFirstName" className={`form-label text-${mode === 'dark' ? 'light' : 'dark'}`}><b>First Name</b></label>
-                    <input type="text" className={`form-control text-${mode === 'dark' ? 'light' : 'dark'} bg-${mode}`} id="enterFirstName" placeholder="Enter First Name" required onChange={onChange} name='fname' value={detail.fname} />
+                    <input type="text" className={`form-control text-${mode === 'dark' ? 'light' : 'dark'} bg-${mode}`} id="enterFirstName" placeholder="Enter First Name" required onChange={onChange} name='from_name' value={detail.from_name} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="enterLastName" className={`form-label text-${mode === 'dark' ? 'light' : 'dark'}`}><b>Last Name</b></label>
@@ -26,11 +31,11 @@ export default function Contact(props) {
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputEmail" className={`form-label text-${mode === 'dark' ? 'light' : 'dark'}`}><b>Email</b></label>
-                    <input type="email" className={`form-control bg-${mode} text-${mode === 'dark' ? 'light' : 'dark'}`} id="inputEmail" placeholder="mail@someone.com" required onChange={onChange} name='email' value={detail.email} />
+                    <input type="email" className={`form-control bg-${mode} text-${mode === 'dark' ? 'light' : 'dark'}`} id="inputEmail" placeholder="mail@someone.com" required onChange={onChange} name='user_email' value={detail.email} />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <label htmlFor={`feedback`} className={`form-label text-${mode === 'dark' ? 'light' : 'dark'}`}><b>Your Feedback</b></label>
-                <textarea className={`form-control text-${mode === 'dark' ? 'light' : 'dark'} bg-${mode}`} id="feedback" rows="10" placeholder='Enter Your Feedback' required onChange={onChange} name='feedback' value={detail.feedback} />
+                <textarea className={`form-control text-${mode === 'dark' ? 'light' : 'dark'} bg-${mode}`} id="feedback" rows="10" placeholder='Enter Your Feedback' required onChange={onChange} name='message' value={detail.message} />
                 <div className="col-12">
                     <button type="submit" className={`btn btn-${mode === 'light' ? 'outline-' : ''}primary`}>Send Feedback</button>
                 </div>
--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Feedback(props) {
   const { mode } = props;
-  const { fname, lname, feedback } = props.details;
+  const { from_name, lname, message } = props.details;
   return (
     props.isSubmit &&
     <>
       <div className={`card text-center mt-4 bg-${mode} mb-2`}>
         <div className="card-header bg-warning ">
-          Thanks For Your Feedback <b>{fname + " " + lname}</b>
+          Thanks For Your Feedback <b>{from_name + " " + lname}</b>
         </div>
         <div className="card-body">
-          <p className={`card-text text-${mode === 'dark' ? 'light' : 'dark'}`}>{feedback}.<br/><i>In Future</i> We will Consider Your Feedback.</p>
+          <p className={`card-text text-${mode === 'dark' ? 'light' : 'dark'}`}>{message}.<br/><i>In Future</i> We will Consider Your Feedback.</p>
         </div>
       </div>
     </>
